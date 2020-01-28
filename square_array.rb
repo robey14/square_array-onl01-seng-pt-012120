a9bfecdb55bbc2b184408a917cdf03796673f1e0
John Michael Robey <robey14@gmail.com>
2020-01-28 23:48:03 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,11 @@
 
-
 def square_array(array)
-    new_array = []
-    counter = 0
-    
-    while counter < array.length do
-      new_array <<[counter] ** 2
-      counter += 1
-    end
-
-new_array 
-    
+  new_array = []
+  array.each do |n|
+    new_array << n ** 2
+  end
+  new_array
 end
+
+
+    
